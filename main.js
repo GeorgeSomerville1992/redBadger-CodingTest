3a19f224b975781a90440fe9cffc9dec255530b8
George Somerville <georgesomerville1992@gmail.com>
2015-03-03 09:23:06 +0000

    working instructions, linked to process output

--- main.js
+++ main.js
@@ -11,6 +11,13 @@ function RobotMovment(){
 
 }
 
+// factor fall of edge
+// output now works coirindates are in place
+	// chdck that this is right process, write notepad
+	// refactor case state into ulties function
+// add form input
+
+// 
 
 function Robot(startX,startY){
 	this.startX = startX ? startX: 0;
@@ -30,39 +37,49 @@ function Robot(startX,startY){
 			for(var i=0;i<arguments.length; i+=1){
 				output.push(arguments[i])
 			}
-			this.outputCoordinates(output)
+			
 			// concat array and display set corridnates for robot on input page
 			this.startX = x
 			this.startY = y
 			this.orintation = orintation
 			console.log(output)
+
+			this.outputCoordinates(output)
+			// this.outputCoordinates(output)
 		},
 		processInstructions:function(instructions){
 			console.log("MY STARTING X POSITION")
-
+			console.log(arguments.length)
+			console.log(instructions)
 			console.log(this.startX)
-			console.log("my orintation is E ")
+			console.log("my orintation is ")
 			console.log(this.orintation)
 			// set of instructions, string, read string
 			// REFACTOR
+
+			/* loop through string of instrucctions, process each step. return final set of corrdinates*/
 			for(var i=0; i<instructions.length; i+=1){
 				console.log(instructions[i])
 				if(instructions[i] === "F"){
 					switch(this.orintation){
 					case "N":
-						this.startY + 10
+						console.log("gone north")
+						this.startY +=10
 						break
 					case "E":
-						this.startY + 1
+						console.log("gone Eash")
+						this.startX += 1
 						break
 					case "S":
-						this.startY - 10
+						this.startY -= 10
 						break
 					case "W":
-						this.startX - 1
-						break
+						this.startX -= 1
+						break;
 					}
-					console.log(this.orintation)	
+					console.log("GONE FORWARD FACING POSITION OF ORINTATION ============>",this.orintation)
+					console.log(this.startY)	
+					console.log(this.startX)
 				}
 				if(instructions[i] === "R"){
 					switch(this.orintation){
@@ -79,32 +96,40 @@ function Robot(startX,startY){
 						this.orintation = "N"
 						break
 					}
+					console.log("Rotated Right Postion changed to =====>>")
 					console.log(this.orintation)
-				}						}
+				}						
 				if(instructions[i] === "L"){
 					switch(this.orintation){
 					case "N":
 						this.orintation = "W"
-						break
+						
 					case "E":
 						this.orintation = "N"
-						break
+						
 					case "S":
 						this.orintation = "E"
-						break
+						
 					case "W":
 						this.orintation = "S"
-						break
+					
 					}
+					console.log("Rotated LEFT Postion changed to =====>>")
 					console.log(this.orintation)
 				}
 			}
+			/* input final Coordinates */
+			this.inputCoordinates(this.startX,this.startY,this.orintation)
+
+		},
+		checkRobotPosition:function(){
+			checkPosition()
 		},
 		outputCoordinates:function(output){
 			
 			// churn it out, into a displayable format
 			console.log(output)
-			// concat array???
+			// concat array and display back onto sep form
 		},
 		rotateLeft:function(orintation){
 
@@ -112,6 +137,7 @@ function Robot(startX,startY){
 		rotateRight:function(orintation){
 
 		}
+
 	}
 }
 
@@ -122,5 +148,12 @@ function outputCoordinates(x,y,orintation){
 
 var georgeRobot = new Robot(0,0)
 
-georgeRobot.inputCoordinates(5,6,"E")
-georgeRobot.processInstructions("F")
+georgeRobot.inputCoordinates(5,6,"N")
+georgeRobot.processInstructions("FFRF")
+// 16,26,E, 6
+
+// y is 26, x is 6
+georgeRobot.processInstructions("RFF")
+
+// facing south
+// y is 6
