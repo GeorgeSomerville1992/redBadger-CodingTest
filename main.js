69ae432bf9caca63214ada040e74abf9890ad8e7
George Somerville <georgesomerville1992@gmail.com>
2015-03-05 08:38:15 +0000

    final changes

--- main.js
+++ main.js
@@ -1,53 +1,40 @@
 /* Red Badger coding test */ 
-/* Where I struggled => when I create a new robot object the original is completely lost 
-    and hence being able input last coordinates where robot fell off proved difficult.
-    I had a go at using global arrays and pushed current coordinates into the when the robot 
-    goes of the grid apon instruction step. But this feels dirty and time was pushing on so I decided not to proceed. 
-    maybe there is a way I can save the current Robot object into something then be able to access it from later
+/* will work assuming the user enter's data in the correct format e.g 0 5 N*/
 
-*/
 
 
 var previousCoordinatesArrayX = [],
     previousCoordinatesArrayY = []
 function Robot() {
     /* private properties only returned object has access to */
+    
+    this.upperRightX = 0;
+    this.upperRightY = 0;
+    this.orientation = "";
+    this.x = 0;
+    this.y = 0;  
     var instructions = [],
         inputArray = [],
         inputValue = document.getElementById("coordinateInput").value,
         instructions = document.getElementById("instructionCommands").value,
         outPut = document.getElementById("coordinateOutput"),
-        submitButton = document.getElementById("send-coordinate");
-    this.orientation = "";
-    this.x = 0;
-    this.y = 0;   
+        submitButton = document.getElementById("send-coordinate")
     return {
-        rotateLeftOrRight: function(left) {
-            switch (this.orientation) {
-                case "N":
-                    this.orientation = left ? "W" : "E"
-                    break;
-                case "E":
-                    this.orientation = left ? "N" : "S"
-                    break;
-                case "S":
-                    this.orientation = left ? "E" : "W"
-                    break;
-                case "W":
-                    this.orientation = left ? "S" : "N"
-                    break;
-            }
-            return this.orientation;
-        },
         inputCoordinates: function() {
-            /* push string into input array */
-            for (var i = 0; i < inputValue.length; i += 1) {
-                inputArray.push(inputValue[i]);
-            };
-            this.x = inputArray[0];
-            this.y = inputArray[2];
-            this.orientation = inputArray[4];
+            var boundary = document.getElementById("coordinateBoundary").value;
+            var boundaryValues = boundary.split(" ");
+            this.upperRightX = boundaryValues[0];
+            this.upperRightY = boundaryValues[1];
+
+
+            var value = inputValue.split(" ")
+            this.x = value[0];
+            this.y = value[1];
+            this.orientation = value[2];
             this.processInstructions();
+        },
+        checkInstructions: function(){
+            // IMEPLEMENT THROW ERROR HERE
         },
         processInstructions: function() {
             /* used because javascript concats to string when your add */
@@ -58,6 +45,9 @@ function Robot() {
                 alert("Instructions Must not be greater than 100")
                 // instructions.value = ' '
                 return false;
+            }
+            if(this.upperRightX > 50 || this.upperRightY > 50){
+                alert("invalid grid number, upperight grid must not be greater than 50")
             }
             for (var i = 0; i < instructions.length; i += 1) {
                 if (instructions[i] === "F") {
@@ -86,23 +76,40 @@ function Robot() {
                     this.orientation = this.rotateLeftOrRight(true);
                 }
                 /* set globals to Values for xInt */
-                this.x = xInt
-                this.y = yInt
+                this.x = xInt;
+                this.y = yInt;
                 /*  if checkRobotPosition is false, pass outputCoordinates with lost argument */
-                this.checkRobotPosition() ? this.outputCoordinates() : this.outputCoordinates("LOST")
+                this.checkRobotPosition() ? this.outputCoordinates() : this.outputCoordinates("LOST");
             }
 
         },
+        rotateLeftOrRight: function(left) {
+            switch (this.orientation) {
+                case "N":
+                    this.orientation = left ? "W" : "E"
+                    break;
+                case "E":
+                    this.orientation = left ? "N" : "S"
+                    break;
+                case "S":
+                    this.orientation = left ? "E" : "W"
+                    break;
+                case "W":
+                    this.orientation = left ? "S" : "N"
+                    break;
+            }
+            return this.orientation;
+        },
         checkRobotPosition: function() {
-            if (this.x > 50 || this.x < 0 || this.y > 50 || this.y < 0) {
+            if (this.x > this.upperRightX || this.x < 0 || this.y > this.upperRightY || this.y < 0) {
                 if(previousCoordinatesArrayX[previousCoordinatesArrayX-1] === this.x && previousCoordinatesArrayY[[previousCoordinatesArrayY-1]] === this.y ){
-                    console.log("Not returning lost because this was the same corrdinate as where my friend fell off")
+                    console.log("this current move will be ignored");
                     /* and this is where I stopped */
                     /* need to find a way to back right out of function, back to loop and pass plus 1 to skip position */
                 }
                 // push previous coordinates of last known position to global arrays.
                 // this function is being looped through each instruction step
-                // as how else would I be able to skip the step.
+                // so I would use this to skip the next instrction within the string.
                 previousCoordinatesArrayX.push(this.x)
                 previousCoordinatesArrayY.push(this.y)
                 return false
@@ -127,3 +134,14 @@ function inputCoordinate() {
     var marsRobot = new Robot();
     marsRobot.inputCoordinates();
 }
+
+
+/* to do => 
+    * fix robot scent
+    * implement unit tests and throw error code. (see unit code)
+    * implement validation for correct format Regex. 
+*/
+
+
+
+
---
+++ unitTestsPesdo.js
@@ -0,0 +1,18 @@
+
+
+/* on success...
+
+	// it should have the following format for final coordinates 
+	// N N L 
+	// it should report LOST when it falls of edge
+	// N N L LOST
+	// it should ignore last robot fell LOST position when coordinate is the same
+
+*/
+
+/* on failure
+	
+	// Should throw correct error upon incorrect instruction length
+
+	// Should throw correct error upon incorrect grid size
+*/
